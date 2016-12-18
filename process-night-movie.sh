aa9be35bd09c5f2ebedc9c84abccbdbe9881e879
Nicolas Martignoni <nicolas@martignoni.net>
2016-12-18 19:35:20 +0100

    Script adapted to use avconv for timelapse production

--- process-night-movie.sh
+++ process-night-movie.sh
@@ -38,7 +38,7 @@ ymd=$year1$month1$day1
 
 # Set paths
 homepath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-tmppath="$homepath/allskycam/tmp"
+tmppath="$homepath/allskycam/tmpmov"
 inpathtoday="$homepath/Documents/sky/$year1/$month1/$day1"
 inpathyesterday="$homepath/Documents/sky/$year2/$month2/$day2"
 outpath="$homepath/Documents/media/sky/$year1/$month1"
@@ -67,39 +67,60 @@ if $testmode ; then # Run only when not testing
   exit 0 ## exit without further action
 fi
 
-# Remove previous file with image names, if present
-if [ -f "$tmppath/hours.txt" ]; then
-  rm "$tmppath/hours.txt"
-fi
-touch "$tmppath/hours.txt"
+# Empty temp directory
+rm -r "$tmppath"/*
+# Empty previous file with image names
+> "$tmppath/hours.txt"
 
 # Create list of image names to combine and store it in a temp file "hours.txt"
-for h in `seq $twilightbegin 2359`;
-do
-  echo "$inpathyesterday/"`printf "%04d" "$h"`".jpg" >> $tmppath/hours.txt
+for (( time=$twilightbegin - 1; time < 2359; )); do
+    time="$(date --date="$time + 1 minutes" +'%H%M')"
+    echo "$inpathyesterday/"`printf "%04d" "$time"`".jpg" >> $tmppath/hours.txt
 done
-for h in `seq 0 $twilightend`;
-do
-  echo "$inpathtoday/"`printf "%04d" "$h"`".jpg" >> $tmppath/hours.txt
+for (( time=0; time <= $twilightend; )); do
+    echo "$inpathtoday/"`printf "%04d" "$time"`".jpg" >> $tmppath/hours.txt
+    time=$((10#$time))
+    time="$(date --date="$time + 1 minutes" +'%H%M')"
+    time=$((10#$time))
 done
 
+# exit 0
+
+# for h in `seq $twilightbegin 2359`;
+# do
+#   echo "$inpathyesterday/"`printf "%04d" "$h"`".jpg" >> $tmppath/hours.txt
+# done
+# for h in `seq 0 $twilightend`;
+# do
+#   echo "$inpathtoday/"`printf "%04d" "$h"`".jpg" >> $tmppath/hours.txt
+# done
+
+# Create numbered soft links pointing to image files in source folder
+# Necessary to cope with avconv requirements to process consecutive numbers in file names
+count=0
+while read -r the_filename
+do
+    new=$(printf "%04d.jpg" $count) # 04 pads to length of 3 max 9999 images
+    ln -s $the_filename $tmppath/$new
+#     echo $tmppath/$new
+    let count=count+1
+done < "$tmppath/hours.txt"
+
 # Create timelapse movie in appropriate location
-# 1. Create avi movie file with mencoder
-mencoder -msglevel all=0 mf://@"$tmppath/hours.txt" -mf w=704:h=480:fps=10:type=jpg -ovc lavc -lavcopts vcodec=mpeg4:v4mv:mbd=2:trell -oac copy -o "$outpath/$ymd-night.avi" > /dev/null 2>&1
-# 2. Produce mp4 version with ffmpeg
-avconv -y -i "$outpath/$ymd-night.avi" -vcodec mpeg4 -b 1200k -mbd 2 "$outpath/$ymd-night.mp4" > /dev/null 2>&1
-# 3. Produce ogv version with ffmpeg
-avconv -y -i "$outpath/$ymd-night.avi" -b 1200k -mbd 2 "$outpath/$ymd-night.ogv" > /dev/null 2>&1
-# 4. Copy movies for web publication
+# 1. Create mp4 movie file with avconv
+avconv -y -r 10 -i "$tmppath/%04d.jpg" -vcodec libx264 -crf 20 -g 5 "$outpath/$ymd-night.mp4" # > /dev/null 2>&1
+# 2. Produce ogv version with avconv
+avconv -y -i "$outpath/$ymd-night.mp4" -b 1200k -mbd 2 "$outpath/$ymd-night.ogv" # > /dev/null 2>&1
+# 3. Copy movies for web publication
 cp "$outpath/$ymd-night.mp4" "$homepath/Documents/media/sky/lastnight.mp4"
 cp "$outpath/$ymd-night.ogv" "$homepath/Documents/media/sky/lastnight.ogv"
-# 5. Copy MP4 movie to Dropbox
+# 4. Copy MP4 movie to Dropbox
 bash "$homepath/dropbox_uploader.sh" -q -f "$homepath/dropbox_uploader.cfg" upload "$outpath/$ymd-night.mp4" "$dropboxpath/$ymd-night.mp4" &
-# 6. Remove AVI movie file (not needed)
-rm "$outpath/$ymd-night.avi"
-# 7. Remove movies older than 60 days, then empty directories, to free space on the server
+# 5. Remove movies older than 60 days, then empty directories, to free space on the server
 find "$homepath/Documents/media/sky/" -type f -name "*-night.ogv" -mtime +60 -exec rm {} \;
 find "$homepath/Documents/media/sky/" -type f -name "*-night.mp4" -mtime +60 -exec rm {} \;
 find "$homepath/Documents/media/sky/" -depth -empty -type d -exec rmdir {} \;
+# 6. Empty tmp directory
+rm -r "$tmppath"/*
 
 ## End of script
