71b65c1fab43cca1c3a803a8692ff2ac4f0b22ab
Nicolas Martignoni <nicolas@martignoni.net>
2016-12-18 16:35:31 +0100

    Script adapted to use avconv for timelapse production

--- process-day-movie.sh
+++ process-day-movie.sh
@@ -41,7 +41,7 @@ ymd="$year$month$day"
 
 # Set paths
 homepath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-tmppath="$homepath/allskycam/tmp"
+tmppath="$homepath/allskycam/tmpmov"
 inpath="$homepath/Documents/sky/$year/$month/$day"
 outpath="$homepath/Documents/media/sky/$year/$month"
 dropboxpath="daymovies/$year/$month"
@@ -57,6 +57,27 @@ if $testmode ; then # Run only when not testing
   exit 0 ## exit without further action
 fi
 
+# Create tmp directory for creating timelapse, if not present, and empty it otherwise
+if [ ! -d "$tmppath" ]; then
+    mkdir -p "$tmppath"
+else
+    rm -r "$tmppath"/*
+fi
+
+# Create numbered soft links pointing to image files in source folder
+# Necessary to cope with avconv requirements to process consecutive numbers in file names
+count=0
+ls "$inpath"/*.jpg |
+(
+    # do something with the rest of the files
+    while read not_the_most_recent_file
+    do
+        new=$(printf "%04d.jpg" ${count}) # 04 pads to length of 3 max 9999 images
+        ln -s ${not_the_most_recent_file} "$tmppath/${new}"
+        let count=count+1
+    done
+)
+
 # Create directory for the day, if not present
 # The test should be true only on 00:00 each day
 if [ ! -d "$outpath" ]; then
@@ -69,20 +90,16 @@ if ! $testmode ; then # Run only when not testing
 fi
 
 # Create timelapse movie in appropriate location
-# 1. Create avi movie file with mencoder
-mencoder -msglevel all=0 mf://"$inpath/*.jpg" -mf w=704:h=480:fps=10:type=jpg -ovc lavc -lavcopts vcodec=mpeg4:v4mv:mbd=2:trell -oac copy -o "$outpath/$ymd.avi" > /dev/null 2>&1
-# 2. Produce mp4 version with ffmpeg
-avconv -y -i "$outpath/$ymd.avi" -vcodec mpeg4 -b 1200k -mbd 2 "$outpath/$ymd.mp4" > /dev/null 2>&1
-# 3. Produce ogv version with ffmpeg
-avconv -y -i "$outpath/$ymd.avi" -b 1200k -mbd 2 "$outpath/$ymd.ogv" > /dev/null 2>&1
-# 4. Copy movies for web publication
+# 1. Create mp4 movie file with avconv
+avconv -y -r 10 -i "$tmppath/%04d.jpg" -vcodec libx264 -crf 20 -g 5 "$outpath/$ymd.mp4"
+# 2. Produce ogv version with avconv
+avconv -y -i "$outpath/$ymd.mp4" -b 1200k -mbd 2 "$outpath/$ymd.ogv" #> /dev/null 2>&1
+# 3. Copy movies for web publication
 cp "$outpath/$ymd.mp4" "$homepath/Documents/media/sky/yesterday.mp4"
 cp "$outpath/$ymd.ogv" "$homepath/Documents/media/sky/yesterday.ogv"
-# 5. Copy MP4 movie to Dropbox
+# 4. Copy MP4 movie to Dropbox
 bash "$homepath/dropbox_uploader.sh" -q -f "$homepath/dropbox_uploader.cfg" upload "$outpath/$ymd.mp4" "$dropboxpath/$ymd.mp4" &
-# 6. Remove AVI movie file (not needed)
-rm "$outpath/$ymd.avi"
-# 7. Remove movies older than 60 days, then empty directories, to free space on the server
+# 5. Remove movies older than 60 days, then empty directories, to free space on the server
 find "$homepath/Documents/media/sky/" -type f \( -not -name "*-night.mp4" -a -name "*.mp4" \) -mtime +60 -exec rm {} \;
 find "$homepath/Documents/media/sky/" -type f \( -not -name "*-night.ogv" -a -name "*.ogv" \) -mtime +60 -exec rm {} \;
 find "$homepath/Documents/media/sky/" -depth -empty -type d -exec rmdir {} \;
